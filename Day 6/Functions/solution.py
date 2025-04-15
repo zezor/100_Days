92689756e0bdb3062f2840912eed61408149edc5
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-04-15 15:48:44 +0000

    Update solution.py

--- Day 6/Functions/solution.py
+++ Day 6/Functions/solution.py
@@ -4,3 +4,30 @@ def my_function():
 
 
 my_function()
+
+
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+        
+
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
