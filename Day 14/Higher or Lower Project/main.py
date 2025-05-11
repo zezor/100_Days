99a8f70ead75b3ff53ef9cb025b70c8d110f77c2
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-05-11 13:32:51 -0800

    Update main.py

---
+++ Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,64 @@
+
+import random
+
+## import game_data
+import game_data
+
+# ## import art
+import art
+
+# ## Random choose from the data A
+# random_choice_a = random.randint(0,30)
+# A = random_choice_a
+#
+# ## Random choose from the data B
+# random_choice_b = random.randint(0,30)
+# B = random_choice_b
+#
+#
+# ## compare function
+# def compare_func():
+#     count_a = 0
+#     count_b = 0
+#     count_a = game_data.data[random_choice_a]["follower_count"]
+#     count_b = game_data.data[random_choice_b]["follower_count"]
+#
+#     if count_a > count_b:
+#         A
+#     else:
+#         print(f"Lower {B}")
+#
+
+#
+# ##printing welcome page with art
+#
+# print(art.logo)
+# print(f"Compare {"A"}: {game_data.data[random_choice_a]["name"]} a"
+#       f" {game_data.data[random_choice_a]["description"]} in {game_data.data[random_choice_a]["country"]} ")
+# compare_func()
+#
+# print(art.vs)
+# print(f"With  {"B"}: {game_data.data[random_choice_b]["name"]} a"
+#       f" {game_data.data[random_choice_b]["description"]} in {game_data.data[random_choice_b]["country"]} ")
+#
+# A = input("Your Answer: ")
+
+# Display Art
+
+# Generate random account from the game data
+
+# Format the game data into printable format
+
+# Ask user for a guess
+
+# # Check if user is correct
+##-- Get follower count of each account
+##-- Use if statement to check if the user is correct
+
+# Give user feedback on their guess
+
+# Score keeping
+
+# Make the game repeatable
+
+# Making account at B become account at position A
