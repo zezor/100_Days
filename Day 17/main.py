46519a8ce67c4aaf936702ac4a523429d516b8ac
zezor <kemma2993@gmail.com>
2025-05-28 23:19:06 -0800

    update the virtual resipotory

--- Day 17/main.py
+++ Day 17/main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
 
@@ -15,3 +15,6 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():#Still has questions
     quiz.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score is: {quiz.current_score}/ {quiz.question_number}")
