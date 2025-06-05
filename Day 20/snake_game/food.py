a180785573e545a2ae59a16194f433b3925a4930
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-06-05 14:39:22 +0000

    commit to main

--- Day 20/snake_game/food.py
+++ Day 20/snake_game/food.py
@@ -4,7 +4,7 @@ class Food (Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape()
+        self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
--- Day 20/snake_game/score_board.py
+++ Day 20/snake_game/score_board.py
@@ -1,10 +1,31 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
 class Score (Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.write(f"Score:, {self.score}", align="center", font=("Arial",10, "normal"))
         #self.write((0, 0), True)
         self.goto(x=0, y=270 )
         self.color("white")
+        self.hideturtle()
+        self.update_scoreboard()
+
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font= FONT)
+
+    def game_over(self):
+        self.goto(x=0, y=0)
+        self.color("white")
+        self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
+        self.hideturtle()
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
