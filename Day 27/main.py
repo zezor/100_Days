acc8dc6ea4e111e6b8fbd004c5782b1c955047ea
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-06-23 17:23:22 -0800

    Update main.py

--- Day 27/main.py
+++ Day 27/main.py
@@ -1,12 +1,31 @@
-import tkinter
-windows = tkinter.Tk()
+from tkinter import *
+#import tkinter
+#windows = tkinter.Tk()
+windows = Tk()
 windows.title("My First GUI Programme")
 windows.minsize(width=500, height=300)
 
 
 #Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24,"bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24,"bold"))
+my_label.config(text="New Text")
 my_label.pack(side="left")
+
+#Button
+def buttonclicked():
+    #print(input.get())
+    my_label.config(text = input.get())
+
+button = Button(text = "Click Me", command=buttonclicked)
+button.pack(side="left")
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+input =Entry(width=10)
+print(input.get())
+input.pack(side="left")
+
 
 
 
