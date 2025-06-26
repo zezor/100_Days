78741081e673903096eb7f74f7074a5f5fff4087
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-06-26 16:37:22 +0000

    update virtual day 27

--- Day 29/main.py
+++ Day 29/main.py
@@ -57,7 +57,7 @@ window.config(padx=50, pady=50)
 
 
 #placing canvas for the padlock
-canvas = Canvas(width=200, height=200,bg="white", highlightthickness=2, highlightbackground="black")
+canvas = Canvas(width=200, height=200, highlightthickness=2)
 padlock_img = PhotoImage(file="logo.png")
 canvas.create_image(100,100, image=padlock_img )
 canvas.grid(column= 1, row=0)
