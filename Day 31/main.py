8c8aee9e63d8a81e7e15f108cf6904eee1e5364c
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-07-03 16:29:50 +0000

    update virtual day 31

--- Day 31/main.py
+++ Day 31/main.py
@@ -23,10 +23,17 @@ canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 canvas.create_text(400, 263, text="word", font=("Ariel", 40, "bold"))
 
 canvas.config(bg=BACKGROUND_COLOR)
-canvas.grid(column=0, row=0)
+canvas.grid(column=0, columnspan=2, row=0)
 
-cross_image = PhotoImage("images/wrong.png")
-unknown_button = Button(image=)
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image)
+unknown_button.grid(row=1, column=0)
+
+
+check_image = PhotoImage(file="images/right.png")
+unknown_button = Button(image=check_image)
+unknown_button.grid(row=1, column=1)
+
 
 # front_back = PhotoImage(file="images/card_back.png")
 # canvas.create_image(400, 263, image=front_back)
