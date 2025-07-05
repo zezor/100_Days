3fd86841e00ec2c41d5fccc51ac816082b66946e
zezor <kemma2993@gmail.com>
2025-07-05 15:50:32 -0800

    UPDATE VIRTUAL FOR DAY 31

--- Day 31/main.py
+++ Day 31/main.py
@@ -8,37 +8,34 @@ from pandas import DataFrame
 BACKGROUND_COLOR = "#B1DDC6"
 
 
-
-
+data = pandas.read_csv("data/french_words.csv")
+data_dict = data.to_dict(orient="records")
+current_card = {]
 # ---------------------------- BUTTONS FUNCTIONALITY ------------------------------- #
 def check_button_select():
-    data = pandas.read_csv("data/french_words.csv")
+    rand_num = random.choice(data_dict)
+    word_selected = rand_num["French"]
+    # word =  dict_selected[key1]
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=word_selected)
 
-    data_dict = data.to_dict(orient="records")
-    rand_num = random.randint(0, 40)
-    dict_selected = data_dict[rand_num]
-
-    key1 = "French"
-    key2 = "English"
-    word =  dict_selected[key1]
-    canvas.create_text(400, 263,text="")
-    canvas.create_text(400, 263, text=word, font=("Ariel", 40, "bold"))
-
-# for key  value in data_dict[rand_num]
-
-
+def flip_card():
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=word_selected)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+window.after(3000, func=flip_card)
+
 
 #CANVAS WITH IMAGES AND TEXT
 canvas = Canvas(width=800, height=526, highlightthickness=2)
 front_card = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=front_card)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 40, "bold"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 40, "bold"))
 
 canvas.config(bg=BACKGROUND_COLOR)
 canvas.grid(column=0, columnspan=2, row=0)
@@ -59,17 +56,6 @@ check_button.grid(row=1, column=1)
 # front_back = PhotoImage(file="images/card_back.png")
 # canvas.create_image(400, 263, image=front_back)
 # canvas.grid(column=1, row=1)
-
-
-
-
-
-
-
-
-
-
-
 
 
 window.mainloop()
