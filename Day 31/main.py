1e3015fa1906473dd632ac26165a2549cba7d4d2
zezor <kemma2993@gmail.com>
2025-07-05 17:42:27 -0800

    UPDATE VIRTUAL FOR DAY 31

--- Day 31/main.py
+++ Day 31/main.py
@@ -6,34 +6,53 @@ from tkinter import *
 from pandas import DataFrame
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
-data = pandas.read_csv("data/french_words.csv")
-data_dict = data.to_dict(orient="records")
-current_card = {]
 # ---------------------------- BUTTONS FUNCTIONALITY ------------------------------- #
 def check_button_select():
-    rand_num = random.choice(data_dict)
-    word_selected = rand_num["French"]
-    # word =  dict_selected[key1]
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=word_selected)
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French",fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(background_card, image=front_card)
+    flip_timer = window.after(3000, func=flip_card)
 
 def flip_card():
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=word_selected)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(background_card, image=back_card)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False) #index set to False to delete index in dataframe
+    check_button_select()
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 
 #CANVAS WITH IMAGES AND TEXT
-canvas = Canvas(width=800, height=526, highlightthickness=2)
+canvas = Canvas(width=800, height=526, highlightthickness=0)
 front_card = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front_card)
+back_card =PhotoImage(file="images/card_back.png")
+background_card = canvas.create_image(400, 263, image=front_card)
+# back_image = canvas.create_image(400, 263, image=background_card)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 40, "bold"))
 
@@ -44,18 +63,16 @@ canvas.grid(column=0, columnspan=2, row=0)
 
 #BUTTONS
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image)
+unknown_button = Button(image=cross_image, command=check_button_select)
 unknown_button.grid(row=1, column=0)
 
 
 check_image = PhotoImage(file="images/right.png")
-check_button = Button(image=check_image, command=check_button_select)
+check_button = Button(image=check_image, command=is_known)
 check_button.grid(row=1, column=1)
 
-
-# front_back = PhotoImage(file="images/card_back.png")
-# canvas.create_image(400, 263, image=front_back)
-# canvas.grid(column=1, row=1)
+#calling out the function below to start with a french word
+check_button_select()
 
 
 window.mainloop()
